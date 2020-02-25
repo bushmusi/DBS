61e93106d5c9ea34ee01daf8fa7c154f37a3dcff
bushmusi <bushera.mestofa@gmail.com>
2020-02-25 22:52:33 +0300

    I added the Authentication and Password Reset functions

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bushera.mestofa@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Afro-Habesh brokery agency'),
     ],
 
     /*
